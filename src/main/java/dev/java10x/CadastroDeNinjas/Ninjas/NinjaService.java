36bf7a4c6d77f18850a55f4938902e055e04a769
Douglas Machado <douglasvilla23@gmail.com>
2025-03-26 23:00:02 -0300

    feat: Crias as classes missoesService e NinjaService e suas rotas com o controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -0,0 +1,25 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    public NinjaModel listarNinjaPorId(Long id){
+        Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
+    }
+}
