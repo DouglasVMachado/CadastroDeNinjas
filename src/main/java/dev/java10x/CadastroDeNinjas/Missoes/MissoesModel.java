5bfd9d964a20e78bf836b5e201a3dbac1bb68694
Douglas Machado <douglasvilla23@gmail.com>
2025-03-26 23:04:26 -0300

    Fix: coloca a annotion @JsonIgnore para resolver o problema de serializaçao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,5 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -20,7 +21,10 @@ public class MissoesModel {
     private int id;
     private String nome;
     private String dificuldade;
+
+
     @OneToMany(mappedBy = "missoes")
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 
 }
