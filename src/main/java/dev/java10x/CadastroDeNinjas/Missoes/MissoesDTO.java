82630a09910812abb51573d9f3424a52e1f41596
Douglas Machado <douglasvilla23@gmail.com>
2025-04-05 00:12:09 -0300

    Feat: Cria as classes MissoesDTO e MissoesMapper para ajudar a rota e segurança

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesDTO.java
@@ -0,0 +1,22 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissoesDTO {
+
+    private Long id;
+    private String nome;
+    private String dificuldade;
+    private List<NinjaModel> ninjas;
+
+}
